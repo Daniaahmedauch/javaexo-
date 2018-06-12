4b3327d6d02f6fa8df01202853c64a23ae95af0f
Daniaamhedauch <94daniak@live.fr>
2018-06-12 13:56:54 +0200

    commito

---
+++ tableau.js
@@ -0,0 +1,12 @@
+
+	
+	var tableau = ['janvier', 'fevrier', 'mars', 'Avril ' ,'citron', 'mai', 'juin','juillet','août','sêptembre','octobre','novembre','decembre'] ; // première méthode
+   
+alert(tableau[2]); // Affiche "mars"
+
+    alert(tableau[5]); // Affiche ""
+    alert(tableau[10]);
+alert(tableau.join(""));
+
+var array = ['Pomme','Cerise','Tomate','Citron','courgette',['Orange','Poivron']] ;
+alert(array.join(""));
