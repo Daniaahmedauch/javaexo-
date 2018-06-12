370d227acf4e4da8b6ac950bc708f95ca57a2fb6
Daniaamhedauch <94daniak@live.fr>
2018-06-12 16:36:30 +0200

    commitokk

--- tableau.js
+++ tableau.js
@@ -1,12 +1,56 @@
-
+/*
 	
-	var tableau = ['janvier', 'fevrier', 'mars', 'Avril ' ,'citron', 'mai', 'juin','juillet','août','sêptembre','octobre','novembre','decembre'] ; // première méthode
+	var mois = ['janvier', 'fevrier', 'mars', 'Avril ' ,'citron', 'mai', 'juin','juillet','août','sêptembre','octobre','novembre','decembre'] ; // première méthode
    
-alert(tableau[2]); // Affiche "mars"
+alert(mois[2]); // Affiche "mars"
 
-    alert(tableau[5]); // Affiche ""
-    alert(tableau[10]);
-alert(tableau.join(""));
+    alert(mois[5]); // Affiche ""
+    alert(mois[10]);
+alert(mois.join(""));
 
-var array = ['Pomme','Cerise','Tomate','Citron','courgette',['Orange','Poivron']] ;
+var array = ['Pomme','Tomate','poire','Citron','courgette',['Orange','Poivron']] ;
 alert(array.join(""));
+
+
+function exo1()
+{
+alert(true);
+}
+exof()*/
+/////////////////////////////////////////////
+function exo2(vim)
+{
+var vim = "message "
+alert (vim)
+}
+exo2()
+
+function exo3(lun)
+{
+	var lun ="koral"
+	alert(lun)
+	var luni ="korali"
+	alert(lun)
+}
+exo3()
+
+ function exo4(moi)
+{
+	var x =prompt("number 1");
+	var y = prompt("number 2");
+	if (x>y) 
+	{
+     alert("number 1 est plus grand");
+    }
+    if (x<y) 
+	{
+     alert("number 1 est plus petite");
+    }
+    if (x==y) 
+	{
+     alert("les deux nombres sont identiques");
+    }
+       
+    
+  }
+exo4()
